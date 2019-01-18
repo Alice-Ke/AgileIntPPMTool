b7d0fa15d85a0748956fb576f3cdfec016e92692
Alice-Ke <pu.ke@sap.com>
2019-01-18 09:58:16 -0800

    add validation

--- ppmtool/src/main/java/io/agileintelligence/ppmtool/domain/Project.java
+++ ppmtool/src/main/java/io/agileintelligence/ppmtool/domain/Project.java
@@ -1,8 +1,12 @@
 package io.agileintelligence.ppmtool.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.util.Date;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 
 @Entity
@@ -11,14 +15,27 @@ public class Project {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+  @NotBlank(message = "Project name is required")
   private String projectName;
+  @NotBlank(message = "Project Identifier is required")
+  @Size(min = 4, max = 5, message = "Please use 4 to 5 characters")
+  @Column(updatable = false, unique = true)
   private String projectIdentifier;
+  @NotBlank(message = "Project description is required")
   private String description;
+  @JsonFormat(pattern = "yyyy-mm-dd")
+  private Date start_date;
+  @JsonFormat(pattern = "yyyy-mm-dd")
+  private Date end_date;
+  @JsonFormat(pattern = "yyyy-mm-dd")
+  private Date created_at;
+  @JsonFormat(pattern = "yyyy-mm-dd")
+  private Date updated_at;
 
   public Long getId() {
     return id;
   }
-""
+
   public String getProjectName() {
     return projectName;
   }
@@ -79,11 +96,6 @@ public class Project {
     this.updated_at = updated_at;
   }
 
-  private Date start_date;
-  private Date end_date;
-
-  private Date created_at;
-  private Date updated_at;
 
   public Project() {
 
