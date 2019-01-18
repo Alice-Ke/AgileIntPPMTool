e6238faadf88a764e7d39142758d00c01dab9309
Alice-Ke <pu.ke@sap.com>
2019-01-18 11:09:25 -0800

    branch5

--- ppmtool/src/main/java/io/agileintelligence/ppmtool/web/ProjectController.java
+++ ppmtool/src/main/java/io/agileintelligence/ppmtool/web/ProjectController.java
@@ -3,11 +3,14 @@ package io.agileintelligence.ppmtool.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 import io.agileintelligence.ppmtool.domain.Project;
 import io.agileintelligence.ppmtool.services.ProjectService;
@@ -21,7 +24,10 @@ public class ProjectController {
 
   // shortcut for @RequestMapping(method = RequestMethod.GET)
   @PostMapping("")
-  public ResponseEntity<Project> creaateNewProject(@RequestBody Project project){
+  public ResponseEntity<?> creaateNewProject(@Valid @RequestBody Project project, BindingResult result){
+    if( result.hasErrors()){
+      return new ResponseEntity<String>("Invalid object", HttpStatus.BAD_REQUEST);
+    }
     Project project1 = projectService.saveOrUpdateProject(project);
     return new ResponseEntity<Project>(project, HttpStatus.CREATED);
   }
